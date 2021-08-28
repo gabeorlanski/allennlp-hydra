29cbe01f4d375f542ba987b1387dc289ce3e0dd6
Gabe <gabeorlanski@gmail.com>
2021-08-28 10:21:54 -0400

    Initial Compose finished

---
+++ allennlp_hydra/commands/compose_config.py
@@ -0,0 +1,93 @@
+import argparse
+import json
+import logging
+from pathlib import Path
+from typing import Dict
+
+from allennlp.commands.subcommand import Subcommand
+import hydra
+from omegaconf import OmegaConf
+from overrides import overrides
+
+logger = logging.getLogger(__name__)
+
+
+@Subcommand.register("compose")
+class ComposeConfig(Subcommand):
+    @overrides
+    def add_subparser(self, parser: argparse._SubParsersAction) -> argparse.ArgumentParser:
+        description = """Compose a config with Hydra"""
+        subparser = parser.add_parser(self.name, description=description, help=description)
+
+        subparser.add_argument("config_path",
+                               type=str,
+                               help="Path to the config directory.")
+
+        subparser.add_argument("config_name",
+                               type=str,
+                               help="Name of the config file to use.")
+        subparser.add_argument("job_name",
+                               type=str,
+                               help="Name of the job.")
+
+        subparser.add_argument(
+            "-s",
+            "--serialization-dir",
+            required=True,
+            type=str,
+            help="Directory to save the config to. The name of the config will "
+                 "be `{config_name}.json`"
+        )
+        subparser.set_defaults(func=compose_config_from_args)
+
+        return subparser
+
+
+def compose_config_from_args(args: argparse.Namespace) -> Dict:
+    """
+    Wrapper for compose so that it can be called with `argparse` arguments from
+    the CLI.
+    # Parameters
+    args: `argparse.Namespace`
+        The parsed args from `argparse`.
+
+    # Returns
+
+    `Dict`: The composed config.
+
+    """
+    return compose_config(
+        config_path=args.config_path,
+        config_name=args.config_name,
+        job_name=args.job_name,
+        serialization_dir=args.serialization_dir
+    )
+
+
+def compose_config(
+        config_path: str,
+        config_name: str,
+        job_name: str,
+        serialization_dir: str
+) -> Dict:
+    # Make the config path relative to the location of THIS file. I.E. make it
+    # relative to the `allennlp_hydra/commands` subdirectory.
+    config_path = Path(config_path).absolute().resolve()
+    if not config_path.exists():
+        raise ValueError(f"Config path '{config_path}' does not exist")
+    if not config_path.is_dir():
+        raise ValueError(f"Config path '{config_path}' is not a directory")
+
+    # Compose the config with hydra.
+    with hydra.initialize_config_dir(config_dir=str(config_path), job_name=job_name):
+        cfg = hydra.compose(config_name=config_name)
+
+    # cfg is a `DictConfig` object, so we need to convert it to a normal dict
+    # using OmegaConf in order to save it.
+    cfg = OmegaConf.to_object(cfg)
+    serialization_dir = Path(serialization_dir)
+    with serialization_dir.joinpath(f"{config_name}.json").open('w',encoding='utf-8' )as cfg_file:
+        # Add the extra options for readability.
+        json.dump(cfg, cfg_file, indent=True, sort_keys=True)
+
+    return cfg
---
+++ tests/commands/compose_test.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pytest
+import json
+
+from allennlp.commands import Subcommand
+
+from allennlp_hydra.utils.testing import BaseTestCase
+from allennlp_hydra.commands import compose_config
+
+
+class TestComposeCommand(BaseTestCase):
+    """
+    Tests for the compose command.
+    """
+
+    def test_is_registered(self):
+        """
+        Test if compose is registered
+        """
+        assert "compose" in Subcommand.list_available()
+        assert Subcommand.by_name('compose') == compose_config.ComposeConfig
+
+    def test_simple_config(self):
+        """
+        Test creating a simple config with compose.
+        """
+        result = compose_config.compose_config(
+            config_path=str(self.FIXTURES_ROOT.joinpath('conf').relative_to(Path.cwd())),
+            config_name="simple_config",
+            serialization_dir=str(self.TEST_DIR.absolute().resolve()),
+            job_name="test_simple_config"
+        )
+
+        # Test that it saved the config to the output directory
+        output_config = self.TEST_DIR.joinpath('simple_config.json')
+        assert output_config.exists()
+
+        # Read the contents of that file and check it is equal to what was
+        # returned.
+        saved_config = json.loads(output_config.read_text('utf-8'))
+        assert saved_config == result
+
+        # Load the expected config.
+        expected = json.loads(
+            self.FIXTURES_ROOT.joinpath("expected_configs/simple_config.json").read_text('utf-8')
+        )
+        assert result == expected
