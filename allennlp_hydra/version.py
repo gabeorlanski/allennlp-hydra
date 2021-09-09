f570e4a0e0e2d51ff959353e7de7ef891157e56a
Gabe <gabeorlanski@gmail.com>
2021-09-09 13:16:48 -0400

    Changed Version File

--- allennlp_hydra/version.py
+++ allennlp_hydra/version.py
@@ -4,8 +4,8 @@ https://github.com/allenai/allennlp-models/blob/main/allennlp_models/version.py
 """
 _MAJOR = "0"
 _MINOR = "0"
-_PATCH = "6"
+_PATCH = "7"
 
-VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
-VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _PATCH)
+VERSION_SHORT = f"{_MAJOR}.{_MINOR}"
+VERSION = f"{VERSION_SHORT}.{_PATCH}"
 __version__ = VERSION
